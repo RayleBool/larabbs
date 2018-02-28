9d760639ddbc8ed091eca4ab4745c9ef7a2c2f1d
leiyisheng <leiyisheng81@163.com>
2018-02-28 13:24:47 +0800

    permissions index

--- routes/api.php
+++ routes/api.php
@@ -108,6 +108,10 @@ $api->version('v1', [
 
             $api->patch('user/read/notifications', 'NotificationsController@read')
             ->name('api.user.notifications.read');
+
+            $api->get('user/permissions', 'PermissionsController@index')
+            ->name('api.user.permissions.index');
+
         });
     });
 });
